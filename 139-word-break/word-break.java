dd39b107dc1b320deebdbb7395fdb66b8bf09d66
Lauren Kwon <160813125+isoo0301@users.noreply.github.com>
2025-11-06 12:46:09 -0600

    Time: 9 ms (18.95%) | Memory: 46.1 MB (5.36%) - LeetSync

---
+++ 139-word-break/word-break.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        int n = s.length();
+        boolean[] dp = new boolean[n+1];
+        dp[0] = true;
+        for(int i = 1; i <= n; i++){
+            for(int j = 0; j < i; j++){
+                String ss = s.substring(j, i);
+                if(dp[j] && set.contains(ss)){
+                    dp[i]=true;
+                    break;
+                } 
+            }
+        }
+        return dp[n];
+    }
+}
