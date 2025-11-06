70d2270bbe9d2ce7f30a1b2517fdfc9ca07a17be
Lauren Kwon <160813125+isoo0301@users.noreply.github.com>
2025-11-06 17:53:02 -0600

    Time: 0 ms (100.00%) | Memory: 41.7 MB (8.48%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] paths = new int[m][n];
+        for(int[] arr: paths){
+            Arrays.fill(arr, 1);
+        }
+
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                paths[i][j] = paths[i-1][j] + paths[i][j-1];
+            }
+        }
+        return paths[m-1][n-1];
+    }
+}
